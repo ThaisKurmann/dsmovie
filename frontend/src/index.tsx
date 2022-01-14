a18c1d37ef09a175cf138a0e122d60d22118afac
ThaisKurmann <thais.martin.ferraz@gmail.com>
2022-01-14 09:07:04 +0100

    Navbar

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root')//acessa o div que esta no documento html
 );
 
 
